d871801c08aab6d7f1f43a4894329be2ef48e764
Kimmy Andersson <cyaspik@gmail.com>
2015-03-01 17:54:36 +0100

    Created CRUD routes for page models, implemented session and flash messages.

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var flash = require('express-flash');
 
 // Helper libraries.
 
@@ -44,6 +47,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(methodOverride(function(req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+app.use(session({ secret: 'TODO', maxAge: null }));
+app.use(flash());
 
 // Helpers.
 
--- routes/pages.js
+++ routes/pages.js
@@ -11,18 +11,100 @@ router.get('*', function(req, res, next) {
     where: {url: req.path},
     include: [models.NavigationItem]
   }).then(function(page) {
-    if (page === null) {
+    if (!page) {
       return next();
+    }
+
+    res.view.set('linkedPage', page);
+    if (!page.NavigationItem) {
+      return res.render('pages/view');
     }
 
     res.view.get('active').push(page.NavigationItem.id);
     navigationHelper.traverseUp(page.NavigationItem, function(parent) {
       res.view.get('active').push(parent.id);
     }).then(function() {
-      res.view.set('linkedPage', page);
-      res.render('website/index');
+      res.render('pages/view');
+    });
+  });
+});
+
+/* GET list pages */
+router.get('/pages', function(req, res, next) {
+  models.Page.findAll().then(function(pages) {
+    res.view.get('linkedPage').title = 'Pages';
+    res.render('pages/index', {pages: pages});
+  });
+});
+
+/* POST create page */
+router.post('/pages', function(req, res, next) {
+  models.Page.create({
+    title: req.body.title,
+    url: req.body.url,
+    content: req.body.content
+  }).then(function(page) {
+    if (!page) {
+      req.flash('danger', 'The page could not be created!');
+      return res.redirect('/pages/new');
+    }
+
+    req.flash('success', 'The page has been created!');
+    res.redirect('/pages/' + page.id + '/edit');
+  });
+});
+
+/* GET new page */
+router.get('/pages/new', function(req, res, next) {
+  res.view.get('linkedPage').title = 'New Page';
+  res.render('pages/edit', {page:models.Page.build({})});
+});
+
+/* GET edit page */
+router.get('/pages/:id/edit', function(req, res, next) {
+  models.Page.find({
+    where: { id: req.params.id }
+  }).then(function(page) {
+    if (!page) {
+      return next();
+    }
+
+    res.view.get('linkedPage').title = 'Edit Page';
+    res.render('pages/edit', {page: page});
+  });
+});
+
+/* PUT update page */
+router.put('/pages/:id', function(req, res, next) {
+  models.Page.find(req.params.id).then(function(page) {
+    if(!page) {
+      return next();
+    }
+
+    page.updateAttributes({
+      title: req.body.title,
+      url: req.body.url,
+      content: req.body.content
+    }).then(function() {
+      req.flash('success', 'The page has been saved!');
+      res.redirect('/pages/' + req.params.id + '/edit');
+    });
+  });
+});
+
+/* DELETE delete page */
+router.delete('/pages/:id', function(req, res, next) {
+  models.Page.find(req.params.id).then(function(page) {
+    if (!page) {
+      return next();
+    }
+
+    page.destroy().then(function(result) {
+      req.flash('success', 'The page has been deleted!');
+      res.redirect('/pages');
     });
   });
 });
 
 module.exports = router;
+
